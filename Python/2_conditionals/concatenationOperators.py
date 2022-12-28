f16a3d8937f43ec423bc2fdf76d4c119822fdd9c
Chema <jmgomezgranados@gmail.com>
2022-12-28 17:34:10 +0100

    ejercicio condicional concatenaciones

---
+++ Python/2_conditionals/concatenationOperators.py
@@ -0,0 +1,21 @@
+president_salary = input("Introduce salario presidente: ")
+print("Salario presidente: " + str(president_salary)) 
+# str funcion que me permite convertir un entero en un string
+# así poder concatenar distintos tipos de datos en mismo formato ya que python es un
+# lenguaje de programación fuertemente tipado y no deja concatenar distintos tipos de datos.
+
+director_salary = input("Introduce salario director: ")
+print("Salario director: " + str(director_salary))
+
+area_manager_salary = input("Introduce salario jefe de area: ")
+print("Salario jefe de area: " + str(area_manager_salary))
+
+admin_salary = input("Introduce salario administrativo: ")
+print("Salario adminstrativo: " + str(admin_salary))
+
+# vamos a comparar los sueldos de los trabajadores con los concatenadores
+
+if admin_salary < area_manager_salary < director_salary < president_salary:
+    print("Está todo correcto")
+else:
+    print("Algo está chungo")
